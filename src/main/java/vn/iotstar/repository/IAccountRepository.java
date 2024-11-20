c2a7734c1ac1c2f4a5e46b67d1411bfb293d3e78
MinhTrung9443 <minhtrungbttv@gmail.com>
2024-11-20 14:49:41 +0700

    fix

--- src/main/java/vn/iotstar/repository/IAccountRepository.java
+++ src/main/java/vn/iotstar/repository/IAccountRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import vn.iotstar.entity.Account;
 
-public interface IAccountRepository  extends JpaRepository<Account, Long> {
+public interface IAccountRepository  extends JpaRepository<Account, Integer> {
 
 	Account findByUsername(String username);
 
