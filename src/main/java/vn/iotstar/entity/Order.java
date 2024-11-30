d8c832010b37f984e3d97fdee4fdb0fa5cab93da
MinhTrung9443 <minhtrungbttv@gmail.com>
2024-11-30 16:56:55 +0700

    update csdl

--- src/main/java/vn/iotstar/entity/Order.java
+++ src/main/java/vn/iotstar/entity/Order.java
@@ -14,6 +14,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
@@ -44,13 +45,17 @@ public class Order implements Serializable{/**
 	@JsonManagedReference
 	private List<OrderLine> lines;
 	
-	@OneToMany(mappedBy = "order",cascade = CascadeType.ALL)
+	@OneToOne(mappedBy = "order",cascade = CascadeType.ALL)
 	@JsonManagedReference
-	private List<Voucher> vouchers;
+	private Voucher vouchers;
 
 	
 	@OneToOne(cascade = CascadeType.ALL)
 	@JoinColumn(name = "paymentId")
 	@JsonManagedReference
 	private Payment payment;
+	
+	@ManyToOne
+	@JoinColumn(name = "deliveryId")
+	private Delivery delivery;
 }
--- src/main/java/vn/iotstar/entity/Voucher.java
+++ src/main/java/vn/iotstar/entity/Voucher.java
@@ -5,14 +5,13 @@ import java.util.Date;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -38,7 +37,7 @@ public class Voucher implements Serializable{/**
 	private Date endDate;
 	private int active;
 	
-	@ManyToOne()
+	@OneToOne()
 	@JoinColumn(name = "orderId")
 	@JsonBackReference
 	private Order order;
