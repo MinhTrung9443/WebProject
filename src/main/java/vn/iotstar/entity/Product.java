c1042421a49999832ec2b36248a6b69249fdd282
hnhung31 <hnhunght31.10@gmail.com>
2024-11-30 16:10:38 +0700

    Thêm bảng ProductImage

--- src/main/java/vn/iotstar/entity/Product.java
+++ src/main/java/vn/iotstar/entity/Product.java
@@ -8,16 +8,7 @@ import java.util.List;
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.*;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -52,7 +43,6 @@ public class Product implements Serializable{
 	private String volumeOrWeight;
 	private String brandOrigin;
 	private long stock;
-	private String images;
 	private LocalDate warehouseDateFirst = LocalDate.now();
 
 	@ManyToOne()
@@ -86,5 +76,8 @@ public class Product implements Serializable{
 	@JsonManagedReference
 	private List<Favourite> favourite;
 
+    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL)
+    @JsonManagedReference
+    private List<ProductImage> images;
 
 }
--- src/main/java/vn/iotstar/entity/ProductFeedback.java
+++ src/main/java/vn/iotstar/entity/ProductFeedback.java
@@ -5,15 +5,7 @@ import java.util.Date;
 
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
