ad8afbbf8c410de274afba1869e802ed19c607e8
nguyentrang4052 <157662731+nguyentrang4052@users.noreply.github.com>
2024-12-05 01:26:45 +0700

    list.html shipper

--- src/main/java/vn/iotstar/controller/Admin/ManageShipperController.java
+++ src/main/java/vn/iotstar/controller/Admin/ManageShipperController.java
@@ -1,5 +1,17 @@
 package vn.iotstar.controller.Admin;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/Admin/shipper")
 public class ManageShipperController {
+	
+	@GetMapping("")
+	public String home(Model model) {
+		return "Admin/shipper/list";
+	}
 
 }
