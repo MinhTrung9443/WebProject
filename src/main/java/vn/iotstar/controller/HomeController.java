9c89bf958074b2cb979e12e62a6e7eb6381c3124
MinhTrung9443 <minhtrungbttv@gmail.com>
2024-12-15 08:33:38 +0700

    khi đã đăng nhập tự động vào waiting

--- src/main/java/vn/iotstar/controller/HomeController.java
+++ src/main/java/vn/iotstar/controller/HomeController.java
@@ -1,6 +1,7 @@
 package vn.iotstar.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import jakarta.servlet.http.HttpSession;
+import vn.iotstar.entity.Account;
 import vn.iotstar.entity.Category;
 import vn.iotstar.entity.Product;
 import vn.iotstar.service.ICategoryService;
@@ -24,6 +26,12 @@ public class HomeController {
 
 	@GetMapping("")
 	public String showHomePage(Model model, HttpSession session) {
+		
+		Account account = (Account) session.getAttribute("account");
+		if (account != null)
+		{
+			return "redirect:/waiting";
+		}
 		// Lấy tất cả sản phẩm từ database
 		List<Product> topSellingProducts = productService.getTop10BestSellingProducts();
         model.addAttribute("products", topSellingProducts);
